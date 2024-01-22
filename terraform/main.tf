bd947e867399f2b981d870aed3ccdcfde60dda3d
Ekta Thakur <ekta.ca.thakur@gmail.com>
2024-01-22 15:54:32 -0500

    added gitlab.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,18 @@ provider "google" {
   zone    = var.zone
 }
 
-provider "kubernetes" {
-  config_path = "/Users/anujkumarsingh/.kube/config"
+provider "kubernetes" {}
+
+module "gke_auth" {
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = "dotted-byway-411905"
+  cluster_name         = "primary"
+  location             = "us-central1-a"
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig"
 }
 
 module "k8s-workload" {
