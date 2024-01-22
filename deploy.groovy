92488362413ee1d1b3232989ea1f39182804130b
Ekta Thakur <ekta.ca.thakur@gmail.com>
2024-01-22 02:36:21 -0500

    added deploy.grrovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,18 +1,19 @@
 pipeline {
     agent any
     stages {
-       /* stage ('fetch docker image') {
+        stage ('fetch code') {
             steps {
-
+                checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/tektathakur/spring-petclinic']])
             }
-        }*/
+        }
 
         stage ('Deploy application') {
             steps {
                 sh """
-                        terraform init
-                        terraform plan
-                        terraform apply
+                    cd terraform
+                    terraform init
+                    terraform plan
+                    terraform apply --auto-approve
                 """
             }
         }
